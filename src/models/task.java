21e9fb056b8e818e89d6376196f319366be8af65
yui.yamamoto <yamamotoy@md-s.co.jp>
2024-11-22 10:51:56 +0900

    change JPA project create dao(util),dto

---
+++ src/models/task.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class task {
+    @Id
+    // @Column 省略できるのかのテスト
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // ID
+    @Column(name = "content", length = 255, nullable = false)
+    private String content; // タスク内容
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at; // 作成日時
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at; // 更新日時
+
+    // getter, setter
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
